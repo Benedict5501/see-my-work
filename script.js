b15b2c11d8cc34c3ef1bca95a1a0b41fb5bd9cf6
Benedict Adeoye Obafemi <tegaobaemi@gmail.com>
2023-11-27 12:57:00 -0800

    well

---
+++ script.js
@@ -0,0 +1,50 @@
+// alert(987)
+// console.log(987)
+
+// alert('femi')
+// console.log("idowu124'./.;'opo;o;..");
+
+// Staring
+// "femi", 'ade', "1234", 'christiana'
+
+// Number
+// 123, 45, 98, -9, 0.65, .698, 76.765
+
+// Boolean
+// true, false
+// console.log(true);
+
+// Array
+// [1, "hello", true]
+
+
+// var earlystudent = "killerman"
+// console.log(earlystudent);
+
+// console.log(2=='2');
+// console.log(245=245);
+
+ var firstNumber = prompt("enter the first number")
+ var secondNumber = prompt("enter the second number")
+ var answer = Number(firstNumber) + Number(secondNumber)
+ console.log(answer);
+
+// console.log('emma' + 'john');
+// console.log(7%4);
+// console.log(15+15);
+// console.log(20-5);
+// console.log(2/6);
+// console.log(2*5);
+// console.log(2**9);
+
+// var firstNumber = prompt('enter the first number')
+// console.log(firstNumber ** 10);
+// var secondNumber = prompt('enter the second number')
+// var answer = firstNumber ** secondNumber
+// console.log(answer);
+
+
+
+
+
+
